788c0eca943944f38d9e30d73e058f0b40b5f44b
cqgong <cgong@ebi.ac.uk>
2021-11-19 14:54:27 +0000

    using @Value annotation to inject properties

--- src/main/java/org/reactome/server/restfulapi/RestfulApiConfiguration.java
+++ src/main/java/org/reactome/server/restfulapi/RestfulApiConfiguration.java
@@ -3,27 +3,35 @@ package org.reactome.server.restfulapi;
 import org.gk.persistence.MySQLAdaptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 public class RestfulApiConfiguration {
     private static final Logger logger = LoggerFactory.getLogger(RestfulApiConfiguration.class);
-    
-    @Autowired
-    private Environment env;
+
+    @Value("${mysql.host}")
+    String mysqlHost;
+
+    @Value("${mysql.db}")
+    String mysqlDatabase;
+
+    @Value("${mysql.user}")
+    String mysqlUser;
+
+    @Value("${mysql.pwd}")
+    String mysqlPassword;
     
     @Bean
-    public MySQLAdaptor getReactomeAnalysisConfig() {
+    public MySQLAdaptor getReactomeAnalysisConfig(@Value("${mysql.host}") String mysqlHost,
+                                                  @Value("${mysql.db}") String mysqlDatabase,
+                                                  @Value("${mysql.user}") String mysqlUser,
+                                                  @Value("${mysql.pwd}") String mysqlPassword) {
         try {
-            MySQLAdaptor dba = new MySQLAdaptor(env.getProperty("mysql.host"), 
-                                                env.getProperty("mysql.db"), 
-                                                env.getProperty("mysql.user"), 
-                                                env.getProperty("mysql.pwd"));
+            MySQLAdaptor dba = new MySQLAdaptor(mysqlHost, mysqlDatabase, mysqlUser, mysqlPassword);
             logger.info("Starting a dumb thread to keep MySQLAdaptor connected to avoid reconnection exception.");
             dba.initDumbThreadForConnection(); // To keep the DBA running to avoid connection error
             return dba;
